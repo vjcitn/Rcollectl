a320d441699e692ccf975980391615b71d1e4d1b
Vince Carey <stvjc@channing.harvard.edu>
2020-11-24 15:42:13 -0500

    graphic in vignette

--- R/cl_parse.R
+++ R/cl_parse.R
@@ -1,13 +1,24 @@
 #' parse a collectl output -- could be conditional on discovered call
+#' @importFrom lubridate as_datetime
+#' @importFrom utils browseURL read.delim
 #' @param path character(1) path to (possibly gzipped) collectl output
+#' @note A lubridate datetime is added as a column.
 #' @examples
-#' lk = cl_parse(system.file("demo/demo_1123.tab.gz", package="Rcollectl"))
+#' lk = cl_parse(system.file("demotab/demo_1123.tab.gz", package="Rcollectl"))
 #' head(lk)
 #' @export
 cl_parse = function(path) {
 	meta = readLines(path, n=14)
 	dat = read.delim(path, skip=14, check.names=FALSE, sep=" ")
 	names(dat) = gsub("\\[(...)\\]", "\\1_", names(dat))
+        dat = revise_date(dat)
 	attr(dat, "meta") = meta
 	dat
+}
+
+revise_date = function(x) {
+ c2 = paste(x[,1], x[,2])
+ pred = gsub("(....)(..)(..)(.*)", "\\1-\\2-\\3\\4", c2)
+ x$sampdate = lubridate::as_datetime(pred)
+ x
 }
---
+++ R/viz.R
@@ -0,0 +1,28 @@
+
+vizdf = function(x) {
+ cpu_active = function(x)
+   data.frame(tm = x$sampdate, xtype="CPU_MEM", 
+     pos="top", value = 100-x$`CPU_Idle%`, type="%CPU active")
+ mem_used = function(x)
+   data.frame(tm = x$sampdate, xtype="CPU_MEM", 
+     pos="bot", value = x$MEM_Used, type="MEM used")
+ net_KB_cum = function(x)
+   data.frame(tm = x$sampdate, xtype="NET_DSK",  
+     pos="top", value = (x$NET_RxKBTot+x$NET_TxKBTot), type="KB NET")
+ dsk_KBwr_cum = function(x)
+   data.frame(tm = x$sampdate, xtype="NET_DSK", 
+     pos="bot", value = cumsum(x$DSK_WriteKBTot), type="Cumul KB disk")
+ 
+ rbind(cpu_active(x), mem_used(x), net_KB_cum(x), dsk_KBwr_cum(x))
+}
+
+#' elementary display of usage data from collectl
+#' @import ggplot2
+#' @param x output of cl_parse
+#' @examples
+#' lk = cl_parse(system.file("demo/demo_1123.tab.gz", package="Rcollectl"))
+#' plot_usage(lk)
+#' @export
+plot_usage = function(x)
+  ggplot(vizdf(lk), aes(x=tm, y=value)) + geom_point() + 
+          facet_grid(vars(type), scales="free")

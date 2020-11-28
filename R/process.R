fe88af6253477b0bb62c393d63bca4b3efafcc9a
Vince Carey <stvjc@channing.harvard.edu>
2020-11-28 06:41:00 -0500

    new code for using processx to control collectl invocation

---
+++ R/process.R
@@ -0,0 +1,53 @@
+#' check for collectl availability
+#' @return logical(1)
+#' @export
+cl_exists = function() {
+  x = try(system2("collectl", "--help", stdout=TRUE))
+  if (!inherits(x, "try-error") && length(x)>30) return(TRUE)
+  FALSE
+}
+
+#' start collectl if possible
+#' @importFrom processx process
+#' @param target character(1) path; destination of collectl report
+#' @return instance of `Rcollectl_process` with components `process` (a processx R6 instance) and
+#' `target` (a file path where collectl results will be written)
+#' @export
+cl_start = function(target = tempfile()) {
+ proc = try(processx::process$new("collectl", args=c("-scdmn", "-P", paste("-f", target, sep=""))))
+ ans = list(process=proc, target=target)
+ class(ans) = "Rcollectl_process"
+ ans
+}
+
+#' print method for Rcollectl process
+#' @param x an entity inheriting from "Rcollectl_process" S3 class
+#' @return invisibly returns the input
+#' @export
+print.Rcollectl_process = function(x, ...) {
+ cat("Rcollectl process object\n  ")
+ print(x$process)
+ cat("  ")
+ cat(x$target, "\n")
+ invisible(x)
+}
+
+#' stop collectl via processx interrupt
+#' @param proc an entity inheriting from "Rcollectl_process" S3 class
+#' @return invisibly returns the input
+#' @export
+cl_stop = function(proc) {
+ stopifnot(inherits(proc, "Rcollectl_process"))
+ proc$process$interrupt()
+ invisible(proc)
+}
+
+#' get full path to collectl report
+#' @param proc an entity inheriting from "Rcollectl_process" S3 class
+#' @return character(1) path to report
+#' @export
+cl_result_path = function(proc) {
+ stopifnot(inherits(proc, "Rcollectl_process"))
+ dn = dirname(proc$target)
+ grep(basename(proc$target), dir(dn, full.names=TRUE), value=TRUE)
+}

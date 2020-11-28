81c30155e258ebc4ec69f0e4c61cd84bfd1076de
Vince Carey <stvjc@channing.harvard.edu>
2020-11-28 06:49:31 -0500

    process handling in place

--- R/process.R
+++ R/process.R
@@ -1,5 +1,7 @@
 #' check for collectl availability
 #' @return logical(1)
+#' @examples
+#' cl_exists()
 #' @export
 cl_exists = function() {
   x = try(system2("collectl", "--help", stdout=TRUE))
@@ -12,6 +14,14 @@ cl_exists = function() {
 #' @param target character(1) path; destination of collectl report
 #' @return instance of `Rcollectl_process` with components `process` (a processx R6 instance) and
 #' `target` (a file path where collectl results will be written)
+#' @examples
+#' if (cl_exists()) {
+#'   zz = cl_start()
+#'   print(zz)
+#'   print(cl_result_path(zz))
+#'   cl_stop(zz)
+#'   zz$process$is_alive()
+#' }
 #' @export
 cl_start = function(target = tempfile()) {
  proc = try(processx::process$new("collectl", args=c("-scdmn", "-P", paste("-f", target, sep=""))))
@@ -22,7 +32,10 @@ cl_start = function(target = tempfile()) {
 
 #' print method for Rcollectl process
 #' @param x an entity inheriting from "Rcollectl_process" S3 class
+#' @param \dots not used
 #' @return invisibly returns the input
+#' @examples
+#' example(cl_start)
 #' @export
 print.Rcollectl_process = function(x, ...) {
  cat("Rcollectl process object\n  ")
@@ -35,6 +48,8 @@ print.Rcollectl_process = function(x, ...) {
 #' stop collectl via processx interrupt
 #' @param proc an entity inheriting from "Rcollectl_process" S3 class
 #' @return invisibly returns the input
+#' @examples
+#' example(cl_start)
 #' @export
 cl_stop = function(proc) {
  stopifnot(inherits(proc, "Rcollectl_process"))
@@ -45,6 +60,8 @@ cl_stop = function(proc) {
 #' get full path to collectl report
 #' @param proc an entity inheriting from "Rcollectl_process" S3 class
 #' @return character(1) path to report
+#' @examples
+#' example(cl_start)
 #' @export
 cl_result_path = function(proc) {
  stopifnot(inherits(proc, "Rcollectl_process"))

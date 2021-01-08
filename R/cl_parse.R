ad2514af02cbbf4dd98cf6a83bfacf0f9272f13e
Vince Carey <stvjc@channing.harvard.edu>
2021-01-08 15:18:02 -0500

    make cl_parse more flexible

--- R/cl_parse.R
+++ R/cl_parse.R
@@ -2,15 +2,16 @@
 #' @importFrom lubridate as_datetime
 #' @importFrom utils browseURL read.delim
 #' @param path character(1) path to (possibly gzipped) collectl output
+#' @param n_to_skip numeric(1) number of initial lines skipped but saved as 'meta' attribute
 #' @return a data.frame
 #' @note A lubridate datetime is added as a column.
 #' @examples
 #' lk = cl_parse(system.file("demotab/demo_1123.tab.gz", package="Rcollectl"))
 #' head(lk)
 #' @export
-cl_parse = function(path) {
-	meta = readLines(path, n=14)
-	dat = read.delim(path, skip=14, check.names=FALSE, sep=" ")
+cl_parse = function(path, n_to_skip=14) {
+	meta = readLines(path, n=n_to_skip)
+	dat = read.delim(path, skip=n_to_skip, check.names=FALSE, sep=" ")
 	names(dat) = gsub("\\[(...)\\]", "\\1_", names(dat))
         dat = revise_date(dat)
 	attr(dat, "meta") = meta

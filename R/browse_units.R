c77a138b6bddf2fc3e8bd0b0b2a3715a606807e0
vjcitn <stvjc@channing.harvard.edu>
2023-03-13 11:34:01 -0400

    use proper protocol for browseURL, with "file://"

--- R/browse_units.R
+++ R/browse_units.R
@@ -6,5 +6,6 @@
 #' }
 #' @export
 browse_units = function() {
-	 browseURL(system.file("units/cl_units.html", package="Rcollectl"))
+	 path = system.file("units", "cl_units.html", package="Rcollectl")
+	 browseURL(paste0("file://", path))
 }

45afe1a0e2f925b3f45bee6deb552a21af0b906e
vjcitn <stvjc@channing.harvard.edu>
2023-04-10 22:11:53 -0400

    responding to review

--- R/browse_units.R
+++ R/browse_units.R
@@ -1,11 +1,12 @@
 #' browse a page defining units of collectl reporting
+#' @param \dots passed to \link{browseURL}
 #' @return side effect is running browseURL
 #' @examples
 #' if (interactive()) {
 #' browse_units()
 #' }
 #' @export
-browse_units = function() {
+browse_units = function(...) {
 	 path = system.file("units", "cl_units.html", package="Rcollectl")
-	 browseURL(paste0("file://", path))
+	 browseURL(paste0("file://", path), ...)
 }
